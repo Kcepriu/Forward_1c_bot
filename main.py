e2504641b98b9fc55b7465f926c23735cf1c9613
Kcepriu <kcepriu@gmail.com>
2021-11-26 13:06:27 +0200

    webhock test 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from forward_bot.bot import bot_instance
+from forward_bot.bot import bot_app
 from forward_bot.configs import WEBHOOK_URL
 
 #from forward_bot.bot import bot_app
 
 app = Flask(__name__)
-app.register_blueprint(bot_instance)
+app.register_blueprint(bot_app)
 
 if __name__ == '__main__':
      # bot_instance.polling()
